bfdd7a7948fcf52a404a3d37ecb614ee5fef5dea
Angiesha <angiesharon25477@gmail.com>
2022-04-10 09:22:31 -0700

    commit 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,26 +2,28 @@
 /**
  * main - Entry point
  *
- * Return: Alwats 0 (success/correct)
+ * Return: Alwatys 0 (success/correct)
  */
 int main(void)
 
 {
 int number;
 
-
 for (number = 48; number <= 57; number++)
 {
 putchar(number);
-}
 
 if (number == 57)
 {
 break;
 }
+
 putchar(',');
 putchar(' ');
 }
 
+putchar('\n');
 
+return (0);
 
+}
