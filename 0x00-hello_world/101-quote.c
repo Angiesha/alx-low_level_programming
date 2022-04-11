60e9c2bb55e48a8732ae045d46cf50ca794647a4
Angiesha <angiesharon25477@gmail.com>
2022-04-11 11:47:25 -0700

    commit 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,9 @@
  *
  * Return: return 1 (Error)
  */
-
 int main(void)
 {
-
-fprintf(stderr, "and that piece of art is userful\" - Dora Korpar, 2015-10-19\n");
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 return (1);
 
