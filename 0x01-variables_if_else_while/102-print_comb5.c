2e40526206d2cb816adf6b8c5530bf4032a27811
Angiesha <angiesharon25477@gmail.com>
2022-04-12 06:49:36 -0700

    commit 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,16 +14,16 @@ int main(void)
 	int a, b, c, d;
 
 	for (i = 0; i < 100; i++)
-        {
+	{
 		a = i / 10; /* tens firstnum */
-		b = i % 10; /* unit firstnum */
+		b = i & 10; /* unit firstnum */
 
-		for (i = 0; j < 100; j++)
+		for (j = 0; j < 100; j++)
 		{
 			c = j / 10; /* tens secondnum */
-			d = j / 10; /* unit secondnum */
+			d = j % 10; /* unit secondnum */
 
-			if (a < c || (a == c && b < d))
+			if (a <c || (a ==c && b < d))
 			{
 				putchar(a + '0');
 				putchar(b + '0');
@@ -31,7 +31,7 @@ int main(void)
 				putchar(c + '0');
 				putchar(d + '0');
 
-				if (!(a == 9 && b == 8))
+				if(!(a == 9 && b == 8))
 				{
 					putchar(',');
 					putchar(' ');
