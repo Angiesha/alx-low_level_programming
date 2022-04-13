bb10ebfec96d38bcec76c423e96b91dcdd4fb6c5
Angiesha <angiesharon25477@gmail.com>
2022-04-13 06:53:55 -0700

    lastdigit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,11 +3,12 @@
 /**
  * main - check the code
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
 	int r;
+
 	print_last_digit(98);
 	print_last_digit(0);
 	r = print_last_digit(-1024);
